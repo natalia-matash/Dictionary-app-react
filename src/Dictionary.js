97dfb0398ac21c69c6c1429aac1cf535fdb32283
Nataliyayaya <natalimatash@gmail.com>
2022-11-16 17:30:32 +0200

    Added dictionary api integtarion

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,17 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 export default function Dictionary() {
    let [keyword, setKeyword] = useState('');
 
+   function hendleResponse(response) {
+      console.log(response.data);
+   }
+
    function searchInfo(event) {
       event.preventDefault();
-      alert(`You search ${keyword}`);
+      let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+      axios.get(apiUrl).then(hendleResponse);
    }
    function handleChange(event) {
       setKeyword(event.target.value);
