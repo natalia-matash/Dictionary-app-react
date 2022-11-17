a00227698cb936ca9292fef312900ce175b45afc
Nataliyayaya <natalimatash@gmail.com>
2022-11-17 11:56:11 +0200

    Rendered the word meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
+import Results from './Results';
 import axios from 'axios';
 
 export default function Dictionary() {
    let [keyword, setKeyword] = useState('');
+   let [results, setResults] = useState(null);
+ 
 
    function hendleResponse(response) {
-      console.log(response.data);
+      setResults(response.data[0]);
    }
 
    function searchInfo(event) {
@@ -22,6 +25,7 @@ export default function Dictionary() {
             <input type='search' onChange={handleChange}/>
             <input type='submit' value='Search'/>
          </form>
+         <Results results={results} />
       </div>
    )
 }
